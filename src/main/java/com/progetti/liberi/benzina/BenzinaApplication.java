c63c0a16e89c06f0aaf3955e77310a7b0c16e038
marco.morandi3 <scuola.mm01@gmail.com>
2025-06-29 23:32:06 +0200

    Init

---
+++ src/main/java/com/progetti/liberi/benzina/BenzinaApplication.java
@@ -0,0 +1,13 @@
+package com.progetti.liberi.benzina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenzinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenzinaApplication.class, args);
+	}
+
+}
